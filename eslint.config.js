79dd30f7b310dfcc3854df8a4be639545ed2a985
Jayden Earles <mail@jaydenearles.com>
2026-05-02 14:12:34 +1000

    Add eslint to the project, including a workflow that checks lint quality

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+
+export default [
+  {
+    ignores: ["node_modules/", ".git/"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+    },
+  },
+];
